cfcb5daed4d8bd000544c3a1bc913986f7321296
Eric Richards <ericrrichards@gmail.com>
2019-01-19 01:31:17 -0500

    Make a hitable interface and scene with two spheres

--- FTracer/Program.fs
+++ FTracer/Program.fs
@@ -7,28 +7,31 @@ open System.Diagnostics
 open Tracer
 
 
-let color r (sphere:Sphere) = 
-    let t= sphere.Hit r
-    if t > 0.0 then 
-        let n = normalize (r.PointAt t - sphere.Center)
-        0.5 * {R=n.X+1.0; G = n.Y+1.0; B = n.Z + 1.0}
-    else
-        let unitDirection = r.Direction.Normalized
-        let t = 0.5 * (unitDirection.Y + 1.0)
-        (1.0-t)*Color.White + t*{R=0.5;G=0.7;B=1.0}       
+let color r (world:IHitable) = 
+    let hit = world.Hit r 0.0 100000.0
+
+    match hit with
+        |Some h ->
+            0.5 * {R=h.Normal.X+1.0; G = h.Normal.Y+1.0; B = h.Normal.Z + 1.0}
+        |None ->
+            let unitDirection = r.Direction.Normalized
+            let t = 0.5 * (unitDirection.Y + 1.0)
+            (1.0-t)*Color.White + t*{R=0.5;G=0.7;B=1.0}               
 
 
 
 [<EntryPoint>]
 let main argv =
-    let nx = 200
-    let ny = 100
+    let nx = 800
+    let ny = 400
     let sb = StringBuilder(sprintf "P3\n%d %d\n255\n" nx ny)
     let lowerLeft = {X = -2.0; Y = -1.0; Z = -1.0}
     let horizontal = {X=4.0; Y = 0.0; Z=0.0}
     let vertical = {X = 0.0; Y = 2.0; Z = 0.0}
     let origin = Vector3.Zero
     let sphere = {Center = -Vector3.UnitZ; Radius=0.5}
+    let sphere2 = {Center = {X=0.0; Y = -100.5; Z = -1.0};Radius=100.0}
+    let world = HitableList ([sphere; sphere2])
     for j = ny-1 downto 0 do
         for i = 0 to nx-1 do
 
@@ -37,8 +40,7 @@ let main argv =
 
             let r = {Origin = origin; Direction = lowerLeft + u*horizontal + v*vertical}
 
-            let col = color r sphere
-            
+            let col = color r world            
             let ir = int(255.99*col.R)
             let ig = int(255.99*col.G)
             let ib = int(255.99*col.B)
--- FTracer/Vector3.fs
+++ FTracer/Vector3.fs
@@ -48,13 +48,46 @@ type Ray =
     {Origin:Vector3; Direction:Vector3}
     member r.PointAt t = r.Origin + t*r.Direction
 
+
+type HitRecord = 
+    { T:float; P:Vector3; Normal:Vector3}
+
+type IHitable =
+    abstract Hit: Ray -> float -> float -> option<HitRecord>
+
+type HitableList(hitables:List<IHitable>) = 
+    member __.List = hitables
+    interface IHitable with
+        member this.Hit r tmin tmax = 
+            let objs = this.List |> List.map (fun h -> h.Hit r tmin tmax )
+            let hits = List.choose id objs
+            if hits.IsEmpty then
+                None
+            else
+                Some(List.minBy (fun h->h.T) hits)
+
+
 type Sphere = 
     {Center:Vector3; Radius:float}
-    member c.Hit r = 
-        let oc = r.Origin - c.Center
-        let a = dot r.Direction r.Direction
-        let b = 2.0 * dot oc r.Direction
-        let c = dot oc oc - c.Radius*c.Radius
-        let discriminant = b*b - 4.0*a*c;
-        if discriminant < 0.0 then -1.0
-        else (-b - Math.Sqrt(discriminant))/(2.0*a)
+    interface IHitable with
+        member this.Hit r tmin tmax = 
+            let oc = r.Origin - this.Center
+            let a = dot r.Direction r.Direction
+            let b =  dot oc r.Direction
+            let c = dot oc oc - this.Radius*this.Radius
+            let discriminant = b*b - a*c;
+
+            if discriminant > 0.0 then
+                let temp = (-b - Math.Sqrt(b*b-a*c))/a
+                if temp < tmax && temp > tmin then
+                    let p = r.PointAt temp
+                    Some({T=temp; P=p; Normal = (p-this.Center)/this.Radius})
+                else 
+                    let temp = (-b + Math.Sqrt(b*b-a*c))/a
+                    if temp < tmax && temp > tmin then
+                        let p = r.PointAt temp
+                        Some({T=temp; P=p; Normal = (p-this.Center)/this.Radius})
+                    else 
+                        None
+            else 
+                None
