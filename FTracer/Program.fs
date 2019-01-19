96e8a03e542275dded3fa01718242a4c8f7aaa27
Eric Richards <ericrrichards@gmail.com>
2019-01-18 23:37:38 -0500

    use color type when building ppm

--- FTracer/Program.fs
+++ FTracer/Program.fs
@@ -4,6 +4,7 @@ open System
 open System.Text
 open System.IO
 open System.Diagnostics
+open Tracer
 
 [<EntryPoint>]
 let main argv =
@@ -11,18 +12,16 @@ let main argv =
     let ny = 100
     let sb = StringBuilder(sprintf "P3\n%d %d\n255\n" nx ny)
     for j = ny-1 downto 0 do
-        //printf "\n%d\n" j
         for i = 0 to nx-1 do
-            //printf "%d," i
-            let r = float(i)/float(nx)
-            let g = float(j)/float(ny)
-            let b = 0.2
-            let ir = int(255.99*r)
-            let ig = int(255.99*g)
-            let ib = int(255.99*b)
+            let col = {R = float(i)/float(nx); G=float(j)/float(ny); B= 0.2}
+            
+            let ir = int(255.99*col.R)
+            let ig = int(255.99*col.G)
+            let ib = int(255.99*col.B)
             sb.Append(sprintf "%d %d %d\n" ir ig ib) |> ignore
 
 
     File.WriteAllText("out.ppm", sb.ToString())
+    Process.Start(ProcessStartInfo("out.ppm",UseShellExecute = true)) |> ignore
     0
     
