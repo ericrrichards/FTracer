193981863e715fa654fec1a5a00c417409bdc25a
Eric Richards <ericrrichards@gmail.com>
2019-01-19 22:20:13 -0500

    Dialectrics

--- FTracer/Program.fs
+++ FTracer/Program.fs
@@ -37,13 +37,14 @@ let rec color r (world:IHitable) (depth:int) =
 let main argv =
     let nx = 400
     let ny = 200
-    let ns = 50
+    let ns = 100
     let sb = StringBuilder(sprintf "P3\n%d %d\n255\n" nx ny)
     let world = HitableList ([
-        Sphere(-Vector3.UnitZ, 0.5,Lambertian(rgb(0.8,0.3,0.3))); 
+        Sphere(-Vector3.UnitZ, 0.5,Lambertian(rgb(0.1,0.2,0.5))); 
         Sphere(vec(0.0,-100.5,-1.0),100.0, Lambertian(rgb(0.8,0.8,0.0)));
-        Sphere(vec(1.0,0.0,-1.0), 0.5, Metal(rgb(0.8,0.6,0.2), 1.0));
-        Sphere(vec(-1.0,0.0,-1.0), 0.5, Metal(rgb(0.8,0.8,0.8), 0.3))
+        Sphere(vec(1.0,0.0,-1.0), 0.5, Metal(rgb(0.8,0.6,0.2), 0.0));
+        Sphere(vec(-1.0,0.0,-1.0), 0.5, Dialectric(1.5))
+        Sphere(vec(-1.0,0.0,-1.0), -0.45, Dialectric(1.5))
     ])
     let cam = Camera()
     let rand = Random()
--- FTracer/materials.fs
+++ FTracer/materials.fs
@@ -17,7 +17,18 @@ let randomInUnitSphere() =
 let reflect v n = 
     v - 2.0 * (dot v n) * n
 
-
+let refract (v:Vector3) n niOverNt = 
+    let uv = v.Normalized
+    let dt = dot uv n
+    let discriminant = 1.0 - niOverNt*niOverNt*(1.0-dt*dt)
+    if (discriminant>0.0) then
+        Some(niOverNt*(uv-n*dt) - n*Math.Sqrt(discriminant))
+    else
+        None
+let schlick cosine refIdx = 
+    let r0 = (1.0-refIdx) / (1.0 + refIdx)
+    let r0 = r0*r0
+    r0 + (1.0-r0)*Math.Pow((1.0-cosine), 5.0)
 
 
 type Lambertian(Albedo:Color)=
@@ -39,3 +50,22 @@ type Metal(Albedo:Color, fuzz:float)=
                 Some {Scattered = scattered; Attenuation = m.Albedo}
             else
                 None
+
+type Dialectric(ri:float) =
+    member __.RefractiveIndex = ri
+    member __.Rand = Random()
+    interface IMaterial with
+        member d.Scatter r_in hit = 
+            let reflected = reflect r_in.Direction hit.Normal
+            let outwardNormal, niOverNt, cosine = 
+                if (dot r_in.Direction hit.Normal) > 0.0 then
+                    -hit.Normal, d.RefractiveIndex, d.RefractiveIndex * (dot r_in.Direction hit.Normal) / r_in.Direction.Length
+                else 
+                    hit.Normal, 1.0 / d.RefractiveIndex, -(dot r_in.Direction hit.Normal) / r_in.Direction.Length
+            let reflectProb, refracted = match refract r_in.Direction outwardNormal niOverNt with
+                | Some ref -> schlick cosine d.RefractiveIndex, ref
+                | None -> 1.0, Vector3.Zero
+            if d.Rand.NextDouble() < reflectProb then
+                Some({Scattered = {Origin = hit.P; Direction=reflected}; Attenuation=Color.White})
+            else 
+                Some({Scattered = {Origin = hit.P; Direction=refracted}; Attenuation=Color.White})
